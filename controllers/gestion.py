73f81647123a3932d8f94f02b648c6fabf629c6e
Daniel Francis <spokerman12@hotmail.com>
2018-07-11 19:26:06 -0400

    "Coordinación" cambia a "Coordinador" en columna "Responsable" en  bitácora. Se toma en cuenta mayúsculas y acentos.

--- controllers/gestion.py
+++ controllers/gestion.py
@@ -10,6 +10,24 @@
 # - Yarima Luciani <13-10770@usb.ve>
 #-----------------------------------------------------------------------------
 
+import re
+
+# Función para corregir nombres en la columna
+# "Responsable" de la bitácora
+def limpia_responsables(texto):
+        
+        dict = {'Coordinación':'Coordinador',
+                'Coordinacion':'Coordinador',
+                'COORDINACIÓN':'Coordinador',
+                'COORDINACION':'Coordinador',
+                }
+        
+        # Se crea una expresion regular para cada clave de dict
+        regex = re.compile("(%s)" % "|".join(map(re.escape, dict.keys())))
+        
+        # Para cada match, se trae el valor en el dict
+        return regex.sub(lambda mo: dict[mo.string[mo.start():mo.end()]], texto)
+
 @auth.requires_login(otherwise=URL('modulos', 'login'))
 def usuarios():
     if(auth.has_membership('ADMINISTRADOR PERSONAL') or auth.has_membership('WEBMASTER')\
@@ -26,6 +44,19 @@ def dependencias():
         table = SQLFORM.smartgrid(db.dependencias,onupdate=auth.archive,links_in_grid=False,csv=False,user_signature=True,paginate=10)
     else:
         table = SQLFORM.smartgrid(db.dependencias,editable=False,deletable=False,csv=False,links_in_grid=False,create=False,paginate=10)
+    
+    
+    o = table.element(_type='submit', _value='%s' % T('Submit'))
+    if o is not None:
+        o['_value'] = T("Agregar")
+        
+    o = table.element(_type='submit', _value='%s' % T('Clear'))
+    if o is not None:
+        o['_value'] = T("Limpiar")
+    
+    o = table.element(_title='View')
+    if o is not None:
+        o['_innerHTML'] = T("Buscar")
     return locals()
 
 @auth.requires_login(otherwise=URL('modulos', 'login'))
@@ -35,4 +66,31 @@ def espacios_fisicos():
         table = SQLFORM.smartgrid(db.espacios_fisicos,onupdate=auth.archive,links_in_grid=False,csv=False,user_signature=True,paginate=10)
     else:
         table = SQLFORM.smartgrid(db.espacios_fisicos,editable=False,deletable=False,csv=False,links_in_grid=False,create=False,paginate=10)
+    return locals()
+
+
+@auth.requires_login(otherwise=URL('modulos', 'login'))
+
+def bitacora_general():
+    '''if(auth.has_membership('ADMINISTRADOR PERSONAL') or auth.has_membership('WEBMASTER')\
+       or auth.has_membership('DIRECTOR') or (auth.user.email == "ulab-calidad@usb.ve")):
+        table = SQLFORM.smartgrid(db.bitacora_general,onupdate=auth.archive, links_in_grid=False,editable=False,create=False,deletable=False,user_signature=True,paginate=10)
+    else:
+        table = SQLFORM.smartgrid(db.bitacora_general,editable=False,deletable=False,csv=False,links_in_grid=False,create=False,paginate=10)
+    #print(db.bitacora_general.fields())
+    '''
+    
+    entradas_query= db().select(db.bitacora_general.ALL)
+    entradas = []
+
+    for elm in entradas_query:
+        responsable = db(db.auth_user.id == elm.created_by).select(db.auth_user.ALL).first()
+
+        entradas.append({
+            'id': elm.id,
+            'accion': elm.f_accion,
+            'fecha': elm.created_on,
+            'responsable': limpia_responsables(responsable.first_name) + " " + responsable.last_name
+        })
+        
     return locals()
